c8bb2a3a8dc8afd073708c4059541ff4f7b84e4e
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-09-17 22:10:30 +0500

    minor bug fixes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB()
 app.use(express.json())
 
 // Routerlardan foydalanish
-app.use('api', orderRoutes)
+app.use('/api', orderRoutes)
 
 // Namuna buyurtmalarni qo'shish
 insertSampleOrders()
