eb0f520679e23d130a176a938f66f339f425a1de
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-09-18 19:14:06 +0500

    bug fixes

--- server.js
+++ server.js
@@ -18,11 +18,15 @@ app.use('/api', orderRoutes)
 
 const PORT = process.env.PORT || 5000
 
-try {
-	app.listen(PORT, () => {
-		console.log(`Server is running on port ${PORT}`)
-	})
-} catch (error) {
-	console.error('Error starting server:', error)
-	process.exit(1)
+const startApp = async () => {
+	try {
+		// await insertSampleOrders()
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`)
+		})
+	} catch (error) {
+		console.error('Error starting server:', error)
+		process.exit(1)
+	}
 }
+startApp()
