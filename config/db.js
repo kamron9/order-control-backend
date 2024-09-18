94d877f429f2026f5bdb9c9402cd25e285938ad1
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-09-18 19:09:14 +0500

    bug fixes

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
 			{
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
+				serverSelectionTimeoutMS: 5000,
 			}
 		)
 		console.log('MongoDB connected')
--- server.js
+++ server.js
@@ -17,14 +17,12 @@ app.use(cors())
 app.use('/api', orderRoutes)
 
 const PORT = process.env.PORT || 5000
-async function start() {
-	try {
-		app.listen(PORT, () => {
-			console.log(`Server is running on port ${PORT}`)
-		})
-	} catch (error) {
-		console.error('Error starting server:', error)
-		process.exit(1)
-	}
+
+try {
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`)
+	})
+} catch (error) {
+	console.error('Error starting server:', error)
+	process.exit(1)
 }
-start()
